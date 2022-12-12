cc11cc438f0f3743168fc547332a4b2ed8c3ff5e
Moritz Angermann <moritz.angermann@gmail.com>
2022-12-12 13:51:14 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,5 +5,6 @@
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
     hydraJobs.hello."x86_64-linux" = packages.x86_64-linux.hello;
+    hydraJobs.hello."x86_64-darwin" = packages.x86_64-darwin.hello;    
   };
 }
