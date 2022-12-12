4407e918eb7442caa18c5931a3ebdef8d5da7203
Moritz Angermann <moritz.angermann@gmail.com>
2022-12-12 13:52:05 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   outputs = { self, nixpkgs }: rec {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
     hydraJobs.hello."x86_64-linux" = packages.x86_64-linux.hello;
     hydraJobs.hello."x86_64-darwin" = packages.x86_64-darwin.hello;    
