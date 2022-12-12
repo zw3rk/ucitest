dd74ba1b9aaff6a93d0a11e21cb882168301a3b9
Moritz Angermann <moritz.angermann@gmail.com>
2022-12-12 12:33:28 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
-  description = "A very very basic flake";
+  description = "A very basic test flake";
 
   outputs = { self, nixpkgs }: rec {
-
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
     hydraJobs.hello."x86_64-linux" = packages.x86_64-linux.hello;
   };
 }
