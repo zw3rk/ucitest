a8a841397903475d25615218141732ef23435a5f
Moritz Angermann <moritz.angermann@gmail.com>
2022-12-16 15:29:19 +0800

    add very basic flake (#5)
    * add very basic flake

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A very basic test flake.";
+
+  outputs = { self, nixpkgs }: rec {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    hydraJobs.hello."x86_64-linux" = packages.x86_64-linux.hello;
+    hydraJobs.hello."x86_64-darwin" = packages.x86_64-darwin.hello;    
+  };
+}
