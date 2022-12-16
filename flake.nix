e4701b985a48ee3e9170f4c8d93963a33671e8ae
Moritz Angermann <moritz.angermann@gmail.com>
2022-12-16 15:51:51 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "A very basic test flake.";
 
+  # these are our outputs, based on some ominous nixpkgs?
   outputs = { self, nixpkgs }: rec {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
